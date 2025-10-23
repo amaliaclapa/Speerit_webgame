0e47d323723de00238f58aff1acb0cf478f5f29c
amaliaclapa <111361174+amaliaclapa@users.noreply.github.com>
2025-10-23 21:59:28 +0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var howToPlayLink = document.getElementById('howToPlay');
+    
+    var rulesBox = document.querySelector('.rulesBox');
+
+    howToPlayLink.addEventListener('click', function() {
+        rulesBox.style.transform = 'scale(1)';
+    });
+
+    document.querySelector('.icon-close').addEventListener('click', function() {
+        rulesBox.style.transform = 'scale(0)';
+    });
+
+    document.querySelector('.playButton').addEventListener('click',function(){
+        document.querySelector('.joinBox').style.transform='scale(1)';
+        document.querySelector('.createGame').style.transform='scale(0)';
+    })
+
+    document.querySelector('.icon-close2').addEventListener('click',function(){
+        document.querySelector('.joinBox').style.transform='scale(0)';
+    })
+
+    document.querySelector('#create').addEventListener('click',function(){
+        document.querySelector('.createGame').style.transform='scale(1)';
+        document.querySelector('.joinBox').style.transform='scale(0)';
+    })
+
+    document.querySelector('.icon-close3').addEventListener('click',function(){
+        document.querySelector('.createGame').style.transform='scale(0)';
+        document.querySelector('.joinBox').style.transform='scale(1)';
+    })
+    document.querySelector('.icon-close4').addEventListener('click',function(){
+        document.querySelector('.joinGame').style.transform='scale(0)';
+        document.querySelector('.joinBox').style.transform='scale(1)';
+    })
+    document.querySelector('#join').addEventListener('click',function(){
+        document.querySelector('.joinGame').style.transform='scale(1)';
+        document.querySelector('.joinBox').style.transform='scale(0)';
+    })
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const gameCode = localStorage.getItem('gameCode');
+    if (gameCode) {
+        document.getElementById('gameCodeDisplay').innerText = `Game Code: ${gameCode}`;
+    }
+});
+
+
+
